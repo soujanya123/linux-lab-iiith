a0163140d6bf80b7d00e22f5932875eedcac1856
kaushik.srinivasan <kaushik.srinivasan@5b58ccf8-e53a-4712-86e9-8fde12058ca0>
2012-12-04 20:01:38 +0000

    still debugging
    git-svn-id: file:///labs/cse09/svn/cse09@28 5b58ccf8-e53a-4712-86e9-8fde12058ca0

--- src/lab/php/login-form.php
+++ src/lab/php/login-form.php
@@ -2,13 +2,13 @@
 
 $login_message = "Login";
 
-$expid=$_REQUEST["expid"];
+echo $_REQUEST["expid"];
 if (isset($_REQUEST['login'])) {
   include_once('ldapexec.php');
 
   if (check_user_account($_REQUEST['login'], $_REQUEST['password'])) {
     session_start();
-    echo $expid;
+//    echo $expid;
     $filename="/home/kaushik/cse09/src/lab/exp".$expid."/interaction-frame.html";
     $_SESSION['authenticated'] = true;
     $_SESSION['username'] = $_REQUEST['login'];
