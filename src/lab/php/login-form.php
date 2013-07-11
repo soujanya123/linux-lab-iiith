499ae883acab978c1c978050ff44bc26097e3f9e
chandan <chandan@5b58ccf8-e53a-4712-86e9-8fde12058ca0>
2013-07-11 09:21:38 +0000

    Added Google Analytics code to login form
    git-svn-id: file:///labs/cse09/svn/cse09@211 5b58ccf8-e53a-4712-86e9-8fde12058ca0

--- src/lab/php/login-form.php
+++ src/lab/php/login-form.php
@@ -25,6 +25,22 @@ if (isset($_REQUEST['login'])) {
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Login Form</title>
 <link href="loginmodule.css" rel="stylesheet" type="text/css" />
+
+ <!-- Google Analytics code starts here -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42255654-1', 'virtual-labs.ac.in');
+  ga('send', 'pageview');
+
+</script>
+
+ <!-- Google Analytics code ends here -->
+
 </head>
 <body>
 <h2><?php echo $login_message;?></h2>
